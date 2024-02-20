2530437b6a5678dfd8a2979d51e74ed7e9f141aa
sanjaydas9027 <sanjaydas9027@gmail.com>
2024-02-20 16:44:35 +0530

    added files

---
+++ spring-boot-app/src/main/java/com/sanjay/DiceApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DiceApplication {
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(DiceApplication.class);
+    app.setBannerMode(Banner.Mode.OFF);
+    app.run(args);
+  }
+}
